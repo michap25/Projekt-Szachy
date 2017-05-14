87dad1b99494cdfed343bcf87e65e3bf0ef96181
michap25 <piechaster@gmail.com>
2017-05-14 22:34:11 +0200

    Projekt Szachy

---
+++ SzachyProjekt/uC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace szachy
+{
+    public partial class uC : UserControl
+    {
+        public int pozX;
+        public int pozY;
+        public uC()
+        {
+            InitializeComponent();
+        }
+
+        private void uC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
